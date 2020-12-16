e30b011f75f16ea3e77285911880f6d78e5765d9
若凡 吳 <norriswu@ptc-nec.com.tw>
2020-12-16 14:52:14 +0800

    add employee-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,13 +18,42 @@
 // }
 
 
+// import { Component, OnInit } from '@angular/core';
+
+// // 引用資料類別
+// import {Employeeinfo} from './employeeinfo';
+
+// // 載入service類別
+// import {EmployeeserviceService} from './employeeservice.service';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+
+// export class AppComponent implements OnInit{
+//    employeelists:Employeeinfo[];
+//    // 建構子
+//    constructor(private employeeService: EmployeeserviceService){}
+
+//    // 初始化, 要先元件implement OnInit
+//    // 一載入就呼叫此函式
+//    ngOnInit(){
+//      this.getEmployees()
+//    };
+
+//    getEmployees():void{
+//      this.employeelists = this.employeeService.getEmployee();
+//    }
+
+//    //觸發事件
+//    doclickevent():void{
+//      alert('test!!!');
+//    }
+// }
+
 import { Component, OnInit } from '@angular/core';
-
-// 引用資料類別
-import {Employeeinfo} from './employeeinfo';
-
-// 載入service類別
-import {EmployeeserviceService} from './employeeservice.service';
 
 @Component({
   selector: 'app-root',
@@ -33,23 +62,12 @@ import {EmployeeserviceService} from './employeeservice.service';
 })
 
 export class AppComponent implements OnInit{
-   employeelists:Employeeinfo[];
+
    // 建構子
-   constructor(private employeeService: EmployeeserviceService){}
+   constructor(){}
 
    // 初始化, 要先元件implement OnInit
-   // 一載入就呼叫此函式
    ngOnInit(){
-     this.getEmployees()
+
    };
-
-   getEmployees():void{
-     this.employeelists = this.employeeService.getEmployee();
-   }
-
-   //觸發事件
-   doclickevent():void{
-     alert('test!!!');
-   }
-
 }
